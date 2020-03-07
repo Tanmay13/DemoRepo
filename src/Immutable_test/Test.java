25f81de4d929e5ee4a40a18248cf46bece3f6703
Tanmay <tanmaykhope77@gmail.com>
2020-03-07 11:25:33 +0530

    New Methode added

--- src/Immutable_test/Test.java
+++ src/Immutable_test/Test.java
@@ -6,4 +6,8 @@ public static void main(String[] args) {
 	System.out.println("New Git changes get added");
 	
 }	
+public void m1()
+{
+System.out.println("New Method get added in Test Class");	
+}
 }
